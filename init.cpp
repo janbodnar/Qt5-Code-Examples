c9bdb0ddc369d68860bc24811e8ab405f166f9b2
Jan Bodnar <jan.bodnar@gmail.com>
2020-12-03 10:10:44 +0100

    Create init.cpp

---
+++ init.cpp
@@ -0,0 +1,22 @@
+#include <QTextStream>
+#include <QDate>
+#include <QTime>
+
+int main(void) {
+
+   QTextStream out(stdout);
+
+   QDate dt1(2020, 4, 12);
+   out << "The date is " << dt1.toString() << endl;
+
+   QDate dt2;
+   dt2.setDate(2020, 3, 3);
+   out << "The date is " << dt2.toString() << endl;
+
+   QTime tm1(17, 30, 12, 55);
+   out << "The time is " << tm1.toString("hh:mm:ss.zzz") << endl;
+
+   QTime tm2;
+   tm2.setHMS(13, 52, 45, 155);
+   out << "The time is " << tm2.toString("hh:mm:ss.zzz") << endl;
+}
