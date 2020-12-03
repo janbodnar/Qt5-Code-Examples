65ca6367041deb4e35f362bc2887229b27a4daa8
Jan Bodnar <jan.bodnar@gmail.com>
2020-12-03 10:19:58 +0100

    Update init.cpp

--- datetime/init.cpp
+++ datetime/init.cpp
@@ -6,14 +6,14 @@ int main(void) {
 
    QTextStream out(stdout);
 
-   QDate dt1(2020, 4, 12);
+   QDate dt1 { 2020, 4, 12 };
    out << "The date is " << dt1.toString() << endl;
 
    QDate dt2;
    dt2.setDate(2020, 3, 3);
    out << "The date is " << dt2.toString() << endl;
 
-   QTime tm1(17, 30, 12, 55);
+   QTime tm1 { 17, 30, 12, 55 };
    out << "The time is " << tm1.toString("hh:mm:ss.zzz") << endl;
 
    QTime tm2;
