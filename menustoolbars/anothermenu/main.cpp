19ec38f401122b4ee3a60ecac72769986bd083d4
Jan Bodnar <jan.bodnar@gmail.com>
2020-12-08 15:01:19 +0100

    Create main.cpp

---
+++ menustoolbars/anothermenu/main.cpp
@@ -0,0 +1,14 @@
+#include "another_menu.h"
+
+int main(int argc, char *argv[]) {
+
+  QApplication app(argc, argv);
+
+  AnotherMenu window;
+
+  window.resize(350, 200);
+  window.setWindowTitle("Another menu");
+  window.show();
+
+  return app.exec();
+}
